190b00c6b9b0ff5cd89d1055f493a055cfb464e2
Jacob Van Buren <jvanbure@andrew.cmu.edu>
2015-11-15 03:55:39 -0500

    boilerplate

--- wifisniffer.py
+++ wifisniffer.py
@@ -1,5 +1,21 @@
 #!/usr/bin/evn python
+
+import threading
 from scapy.all import *
+import subprocess as sp
+import Queue
+
+class Sniffer(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.sp.Popen(['hostapd', 'etc/hostapd/hostapd.conf'])
+        # init stuff here
+
+    def run(self, queue):
+        pass
+
+
+
 def print_summary(pkt):
     if IP in pkt:
         ip_src=pkt[IP].src
