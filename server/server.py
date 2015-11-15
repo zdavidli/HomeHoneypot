ee22273c803bf4c296739ffa80017e9cfaf1831e
Jacob Van Buren <jvanbure@andrew.cmu.edu>
2015-11-15 03:50:09 -0500

    reorganize

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", 8080)
-handler.cgi_directories = ["/"]
+# handler.cgi_directories = ["/"]
  
 httpd = server(server_address, handler)
 httpd.serve_forever()
