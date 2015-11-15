acd7f011fe3d1c41bc19e4345778c5294f6ec51d
Jacob Van Buren <jvanbure@andrew.cmu.edu>
2015-11-15 06:47:07 -0500

    ...

--- push.py
+++ push.py
@@ -5,5 +5,5 @@ pb = Pushbullet(api_key)
 nexus5 = pb.devices[0]
 
 def note(interface, device, mac_addr, notes):
-    print "HomeHoneypot: device {1} detected on {0}".format(device, interface), "MAC address: {0}\n{1}".format(mac_addr, notes))
+    print "HomeHoneypot: device {1} detected on {0}".format(device, interface), "MAC address: {0}\n{1}".format(mac_addr, notes)
     # nexus5.push_note("HomeHoneypot: {1} detected on {0}".format(interface, device), "MAC address: {0}\n{1}".format(mac_addr, notes))
